cc4280c1a0b76c8cc085751223a741ff3e0ab484
Adam Mohammed <13.adamm@gmail.com>
2021-02-01 17:41:17 -0500

    lotide head update

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = function(actual){
+  let one = actual[0]
+  return one
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([13,35345,34534]), 14);
+assertEqual(head([]), "Games");
