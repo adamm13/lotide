9ee28665607fbbdc3e8e6f15193309a1ccb416da
Adam Mohammed <13.adamm@gmail.com>
2021-02-02 16:49:07 -0500

    Comparing two arrays for equality - lotide

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let success = true;
+
+  if (arr1.length !== arr2.length) {
+    success = false //if arrays arent the same length it will fail.
+  }
+
+  for (let i = 0; i < arr1.length; i++) { 
+    if (arr1[i] !== arr2[i]) { //if theyre the same length we then check for the same values within.
+      success = false
+    }
+  }
+  return success;
+}
+
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
