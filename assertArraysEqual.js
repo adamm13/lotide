cf2d50199de02c9ae1846987d3b805e0902f8abe
Adam Mohammed <13.adamm@gmail.com>
2021-02-03 21:49:49 -0500

    updated array equal to print more visible outputs

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,28 @@
-const assertArraysEqual = function(array1, array2)
-{
-  if(array1.length === array2.length){
-    for (var i = 0; i < array1.length; i ++){
-      if(array1[i] !== array2[i]){ 
-        //console.log("Arrays are not the same");
-        return "Arrays are NOT the same";
-      }
+
+const eqArrays = function(arr1, arr2) {
+  let success = true;
+
+  if (arr1.length !== arr2.length) {
+    success = false //if arrays arent the same length it will fail.
+  }
+
+  for (let i = 0; i < arr1.length; i++) { 
+    if (arr1[i] !== arr2[i]) { //if theyre the same length we then check for the same values within.
+      success = false
     }
   }
-  else
-  {//console.log("arrays not same length, therefore not the same");
-    return "Arrays are NOT the same";
-  }
-return "Arrays are the Same";
-
-
+  return success;
 }
 
-//console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
-//console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo)) {
+    console.log(`✅ Assertation Passed: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`🔴 Assertation Failed: ${arrOne} !== ${arrTwo}`);
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
