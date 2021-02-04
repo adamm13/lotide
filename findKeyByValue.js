cce3b2decc4b96c4178da2b90aac1d21359534cd
Adam Mohammed <13.adamm@gmail.com>
2021-02-03 19:05:18 -0500

    see if and input matches the keys in an object

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(genre, show) {
+  for (const key in genre) { 
+    if (genre[key] === show) {
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  horror: "Hannibal"
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Hannibal"), "horror");
