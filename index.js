650eb24fcdea9f327834e7047fd0013dee564844
Adam Mohammed <13.adamm@gmail.com>
2021-02-08 16:31:21 -0500

    updated to include all utility functions (Even ones not being currently utilized)

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const tail = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
+// tail, head, middle, assertArraysEqual, eqArrays, assertObjectsEqual, assertEqual used so far
 
 module.exports = {
   head,
@@ -12,5 +24,18 @@ module.exports = {
   middle,
   assertArraysEqual,
   assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
   eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  tail,
+  takeUntil,
+  without
 };
