30cda720d97444f66523673e7821f671df69d317
Adam Mohammed <13.adamm@gmail.com>
2021-02-08 14:15:58 -0500

    added export to function to test

--- tail.js
+++ tail.js
@@ -1,19 +1,20 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+
+const tail = function(array) {
+  let one = array.slice(-1);
+  return one[0];
 };
 
+/*
 const tail = function(actual) {
   let one = [actual.slice(1)];
   return one;
-};
+};*/
 
+//assertEqual(tail([5, 5]), 5);
+//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(tail([13, 35345, 34534]), 14);
+//assertEqual(tail([]), "Hello");
+//assertEqual(tail([13]), 14);
+//assertEqual(tail([13, 14, 12, 13]), [13]);
 
-assertEqual(tail([5,5]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail([13,35345,34534]), 14);
-assertEqual(tail([]), "Hello");
-assertEqual(tail([13]), 14);
+module.exports = tail;
